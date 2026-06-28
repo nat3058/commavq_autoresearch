6de7aa9261a17032d61b74ab89df6339301053f9
nat3058 <nat3058@gmail.com>
2026-06-28 16:40:00 -0700

    SSTC-OPT: Revert BATCH_SIZE to 64 to optimize 5-minute step frequency

--- commavq_autoresearch/train.py
+++ commavq_autoresearch/train.py
@@ -18,7 +18,7 @@ N_HEAD = 7
 N_EMBD = 448
 
 TOKEN_EMBD_DIM = 64
-BATCH_SIZE = 128         # Batch size per GPU (effective batch size = 256)
+BATCH_SIZE = 64          # Batch size per GPU (effective batch size = 128)
 LEARNING_RATE = 8e-4     # Restored to optimal learning rate
 WEIGHT_DECAY = 0.01
 
